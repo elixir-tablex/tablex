7a43c6506a8fe7c26e49be4de7b13faed9a8f57f
Qiu Hua <qiu@zubale.com>
2023-04-21 10:36:41 -0700

    bump: v0.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Tablex.MixProject do
   def project do
     [
       app: :tablex,
-      version: "0.1.0-alpha.2",
+      version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
